7661d6c195066cbc01417b6bcfc332238f1adfff
Christian Hein <scheiehein@gmail.com>
2021-03-04 14:35:46 +0100

    beginning to work a bit more

--- main.py
+++ main.py
@@ -22,16 +22,28 @@ plt.show()
 from osgeo import gdal
 from mayavi import mlab
 
-ds = gdal.Open('assets/slope.dem')
+ds = gdal.Open('assets/subset_DEM3.dem')
 data = ds.ReadAsArray()
+data = data[:1000, 900:1900]
+data[data <= 0] = data[data > 0].min()
+'''
+for i in range(len(data)):
+    print(data[i])
+    for j in range(len(data[i])):
+        if data[i][j] < 0:
+            data[i][j] = 0
+'''
 
-mlab.figure(size=(640, 800), bgcolor=(0.16, 0.28, 0.46))
+print(data)
 
-mlab.surf(data, warp_scale=0.2) 
+mlab.figure(size=(800, 640), bgcolor=(0.5, 0.5, 0.5))
+
+repres = ["surface", "wireframe", "points"]
+
+mlab.surf(data, colormap='gist_earth', warp_scale=0.5, vmin=100, vmax=250, representation=repres[0], line_width=3.0) 
 
 f = mlab.gcf()
 camera = f.scene.camera
-camera.yaw(45)
+#  camera.yaw(45)
 
 mlab.show()
-
