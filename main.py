3e0da368348aae47bc62bdba362aa9bcffcc5af5
Christian Hein <scheiehein@gmail.com>
2021-03-05 16:41:09 +0100

    reads dem normally now

--- main.py
+++ main.py
@@ -1,49 +1,18 @@
-import matplotlib.pyplot as plt
 from osgeo import gdal
 from mayavi import mlab
 
-'''
-
-src = gdal.Open('assets/cropped10.tif')
-dst = 'assets/mgddem.tif'
-t = 'hillshade'
-slope = gdal.DEMProcessing(dst, src, t, computeEdges = True)
-
-
-rb = slope.GetRasterBand(1).ReadAsArray()
-plt.figure()
-plt.imshow(rb, cmap="gray")
-plt.colorbar()
-plt.show()
-
-'''
-
-
-from osgeo import gdal
-from mayavi import mlab
-
-ds = gdal.Open('assets/subset_DEM3.dem')
-data = ds.ReadAsArray()
-data = data[:1000, 900:1900]
-data[data <= 0] = data[data > 0].min()
-'''
-for i in range(len(data)):
-    print(data[i])
-    for j in range(len(data[i])):
-        if data[i][j] < 0:
-            data[i][j] = 0
-'''
-
+ds = gdal.Open('assets/67M1_sub.dem')
+data = ds.GetRasterBand(1).ReadAsArray()[:-1]
 print(data)
 
 mlab.figure(size=(800, 640), bgcolor=(0.5, 0.5, 0.5))
-
 repres = ["surface", "wireframe", "points"]
-
-mlab.surf(data, colormap='gist_earth', warp_scale=0.5, vmin=100, vmax=250, representation=repres[0], line_width=3.0) 
-
+mlab.surf(data, 
+        colormap='terrain', 
+        warp_scale=0.04, 
+        vmin=0, 
+        representation=repres[2], 
+        line_width=1.0) 
 f = mlab.gcf()
 camera = f.scene.camera
-#  camera.yaw(45)
-
 mlab.show()
