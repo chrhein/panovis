38b3c759afb11b16c4564229c57adc38d9e7238d
Christian Hein <scheiehein@gmail.com>
2021-03-09 20:15:42 +0100

    rendering a kind-of depth map using povray

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from osgeo import gdal
 from mayavi import mlab
+import numpy as np
+import affine
+
 
 ds = gdal.Open('assets/67M1_sub.dem')
 data = ds.GetRasterBand(1).ReadAsArray()[:-1]
 print(data)
+print(data.min(), data.max())
 
-mlab.figure(size=(800, 640), bgcolor=(0.5, 0.5, 0.5))
+indices = np.where(data == data.max())
+ymax, xmax = indices[0][0], indices[1][0]
+print("The highest point is", data[ymax][xmax])
+print("  at pixel location", xmax, ymax)
+
+'''
+affine_transform = affine.Affine.from_gdal(*data.GetGeoTransform())
+lon, lat = affine_transform * (xmax, ymax)
+'''
+
+mlab.figure(size=(800, 640), bgcolor=(0.4, 0.4, 0.4))
 repres = ["surface", "wireframe", "points"]
 mlab.surf(data, 
         colormap='terrain', 
         warp_scale=0.04, 
         vmin=0, 
-        representation=repres[2], 
+        representation=repres[0], 
         line_width=1.0) 
 f = mlab.gcf()
 camera = f.scene.camera
