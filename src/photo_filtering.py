9119c005efb1cc7e953fd7e55b073e0042b6e552
Christian Hein <scheiehein@gmail.com>
2021-04-08 16:08:39 +0200

    improved contour highlighting

--- src/photo_filtering.py
+++ src/photo_filtering.py
@@ -1,20 +1,24 @@
 import cv2
-import matplotlib.pyplot as plt
 
 
 def photo_filtering(input_photo):
     photo = cv2.imread(input_photo)
     smaller = cv2.resize(photo, (0, 0), fx=0.2, fy=0.2)
+    dst = cv2.GaussianBlur(smaller, (3, 3), 0)
+    # darkened = change_brightness(dst, value=-50)
 
-    darkened = change_brightness(smaller, value=-50)
-    dst = cv2.GaussianBlur(darkened, (3, 3), cv2.BORDER_DEFAULT)
+    image = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    edges = cv2.Canny(gray, 200, 255)
 
-    rgb = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
-    grayscale = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
-    edges = cv2.Canny(grayscale, threshold1=200, threshold2=70)
-    cv2.imshow("Canny Image", edges)
+    _, binary = cv2.threshold(edges, 200, 255, cv2.THRESH_BINARY)
+
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    filtered_contours = [x for x in contours if (cv2.contourArea(x) > 5) and (cv2.contourArea(x) < 1000)]
+    image = cv2.drawContours(smaller, filtered_contours, -1, (0, 0, 255), thickness=3)
+    cv2.imshow("contour_detected_image", image)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
 
 def change_brightness(img, value=30):
