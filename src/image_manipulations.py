c998a035b2ebf0ccca6e5c3e1eacc6d1a87994d8
Christian Hein <scheiehein@gmail.com>
2021-12-01 13:00:44 +0100

    broken lines fixed for most panos

--- src/image_manipulations.py
+++ src/image_manipulations.py
@@ -31,8 +31,7 @@ def skeletonize(image):
     image[image != 0] = 255
     _, img = cv2.threshold(image, 1, 255, 0)
     skeleton_image = np.zeros(img.shape, np.uint8)
-    # kernel = np.ones((3, 3), np.uint8)
-    kernel = cv2.getStructuringElement(cv2.MORPH_DILATE, (3, 3))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     while True:
         opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
         temp = cv2.subtract(img, opening)
@@ -40,7 +39,6 @@ def skeletonize(image):
         skeleton_image = cv2.bitwise_or(skeleton_image, temp)
         if cv2.countNonZero(img) == 0:
             break
-    # dilated_image = cv2.dilate(skeleton_image, np.ones((1, 2), np.uint8), iterations=1)
     return skeleton_image
 
 
@@ -49,27 +47,30 @@ def flip(image, direction=0):
     return cv2.flip(image, flipCode=direction)
 
 
-def trim_edges(image):
-    vertical_effect = 1
-    horizontal_effect = 4
-
-    kernel = np.ones((vertical_effect, horizontal_effect), np.uint8)
-    dilated_image = cv2.dilate(image, kernel, iterations=6)
-    eroded_image = cv2.erode(dilated_image, kernel, iterations=7)
-
-    _, thresh_binary = cv2.threshold(eroded_image, 40, 255, cv2.THRESH_BINARY)
+def remove_smaller_contours(image, min_area=100, lb=40, ub=255):
+    _, thresh_binary = cv2.threshold(image, lb, ub, cv2.THRESH_BINARY)
     mask = np.zeros(image.shape[:2], dtype=np.uint8)
     contours, _ = cv2.findContours(
         image=thresh_binary, mode=cv2.RETR_CCOMP, method=cv2.CHAIN_APPROX_SIMPLE
     )
+    [
+        cv2.drawContours(mask, [cnt], 0, (255), -1)
+        for cnt in contours
+        if cv2.contourArea(cnt) > min_area
+    ]
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
-    if len(contours) != 0:
-        [
-            cv2.drawContours(mask, [cnt], 0, (255), -1)
-            for cnt in contours
-            if cv2.contourArea(cnt) > 10000
-        ]
 
-    masked_image = cv2.bitwise_and(eroded_image, mask)
-
-    return skeletonize(masked_image)
+def trim_edges(image):
+    kernel = np.ones((1, 4), np.uint8)
+    dilated_image = cv2.dilate(image, kernel, iterations=6)
+    eroded_image = cv2.erode(dilated_image, kernel, iterations=7)
+    masked_image = remove_smaller_contours(eroded_image, 10000)
+    median = cv2.medianBlur(masked_image, 3)
+    skeletonized = skeletonize(median)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    dilated = cv2.dilate(skeletonized, kernel, iterations=1)
+    masked_image = remove_smaller_contours(dilated, 100)
+    median = cv2.medianBlur(masked_image, 3)
+    return skeletonize(median)
