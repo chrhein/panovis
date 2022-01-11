b3e470a06f5c20f2b7a3ef8a40d94ed0ace10b57
Christian Hein <scheiehein@gmail.com>
2022-01-11 18:53:09 +0100

    view panoramas in browser

--- src/run.py
+++ src/run.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, redirect
+import os
+from flask import Flask, render_template, request, redirect, url_for
 from main import main
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path="/static")
+    UPLOAD_FOLDER = "static/uploads/"
+    app.secret_key = "secret key"
+    app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+    app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
     main_modes = [
         "DEM Rendering",
         "Edge Detection",
@@ -21,11 +26,28 @@ def create_app():
         if request.method == "POST":
             mode = int(request.form["mode-selector"]) - 1
             if mode == 0:
-                return redirect(f"/dem")
+                return redirect(f"/panos")
+            elif mode == 1:
+                return redirect(f"/ed")
+            elif mode == 2:
+                return redirect(f"/fm")
+
+    @app.route("/panos", methods=["POST", "GET"])
+    def select_panorama():
+        pics = sorted(os.listdir("src/static/panoramas/"))
+        return render_template("photo_select.html", pics=pics)
 
     @app.route("/dem")
     def dem():
         return render_template("dem.html")
+
+    @app.route("/ed")
+    def ed():
+        return render_template("ed.html")
+
+    @app.route("/fm")
+    def fm():
+        return render_template("fm.html")
 
     return app
 
