1d6df3b5fbdabd0f0a206a9f5633547d97384c71
Christian Hein <scheiehein@gmail.com>
2022-02-23 12:12:24 +0100

    updated hover info for 3D terrain

--- src/vistools/tplot.py
+++ src/vistools/tplot.py
@@ -6,12 +6,13 @@ def plot_3d(ds_raster, plotpath):
     height_band = ds_raster.read(1)
     w, h = height_band.shape
     resolution = ds_raster.res[0]
-    offset = 15 * resolution
-    trimmed_ds = height_band[int(w/2-offset):int(w/2+offset),
-                             int(h/2-offset):int(h/2+offset)]
+    offset = 200
+    trimmed_ds = flip(height_band[int(w/2-offset):int(w/2+offset),
+                                  int(h/2-offset):int(h/2+offset)], 1)
 
     fig = go.Figure(
-        data=[go.Surface(z=flip(trimmed_ds, 1), colorscale='Fall',
+        data=[go.Surface(z=trimmed_ds, colorscale='Fall',
+                         hovertemplate='<b>Height:</b> %{z:.0f} m<extra></extra>',
                          lightposition=dict(x=100,
                                             y=100,
                                             z=2000),)], )
@@ -23,12 +24,13 @@ def plot_3d(ds_raster, plotpath):
                       template='plotly_white',
                       scene=dict(
                           xaxis=dict(visible=False, showgrid=False,
-                                     zeroline=False,),
+                                     zeroline=False),
                           yaxis=dict(visible=False, showgrid=False,
                                      zeroline=False),
                           zaxis=dict(visible=False, showgrid=False),
                           aspectmode='manual',
                           aspectratio=dict(x=1, y=1, z=1.5/resolution),
+
 
                       ),
 
